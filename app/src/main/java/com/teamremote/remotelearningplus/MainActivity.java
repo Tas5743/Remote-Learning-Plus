c6cc968a9e780d2c521704805fcb77bfad5b59c6
RoyalF4 <Royal2791@gmail.com>
2021-02-20 13:50:17 -0500

    Added Login Page/Google Services.json

--- app/src/main/java/com/teamremote/remotelearningplus/MainActivity.java
+++ app/src/main/java/com/teamremote/remotelearningplus/MainActivity.java
@@ -1,14 +1,97 @@
 package com.teamremote.remotelearningplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtTxtEmail, edtTxtPassword;
+    Button btnLogin;
+    TextView txtRegister;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        edtTxtEmail = findViewById(R.id.edtTxtEmail);
+        edtTxtPassword = findViewById(R.id.edtTxtPassword);
+        txtRegister = findViewById(R.id.txtSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null){
+                    Toast.makeText(MainActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, LoginSuccess.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = edtTxtEmail.getText().toString();
+                String password = edtTxtPassword.getText().toString();
+                if(email.isEmpty()){
+                    edtTxtEmail.setError("Please enter your email.");
+                    edtTxtEmail.requestFocus();
+                }
+                else if(password.isEmpty()){
+                    edtTxtPassword.setError("Please enter your password.");
+                    edtTxtPassword.requestFocus();
+                }
+                else if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Fields are empty!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!(email.isEmpty() && password.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Login Error", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intSuccess = new Intent(MainActivity.this, LoginSuccess.class);
+                                startActivity(intSuccess);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intRegister = new Intent(MainActivity.this,RegisterUser.class);
+                startActivity(intRegister);
+            }
+        });
     }
 }
