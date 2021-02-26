509adbd2e09770c7b406a3cacb27c6e6730ebb7c
RoyalF4 <Royal2791@gmail.com>
2021-02-26 14:45:37 -0500

    Added checks on user input.

--- app/src/main/java/com/teamremote/remotelearningplus/RegisterUser.java
+++ app/src/main/java/com/teamremote/remotelearningplus/RegisterUser.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -16,12 +17,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class RegisterUser extends AppCompatActivity {
 
@@ -37,6 +40,7 @@ public class RegisterUser extends AppCompatActivity {
     }
 
     public void saveUser(View view){
+
         EditText edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
         EditText edtTxtLastName = findViewById(R.id.edtTxtLastName);
         EditText edtTxtInstitution = findViewById(R.id.edtTxtInstitution);
@@ -49,30 +53,62 @@ public class RegisterUser extends AppCompatActivity {
         String txtEmail = edtTxtEmail.getText().toString().trim();
         String txtPassword = edtTxtPassword.getText().toString().trim();
 
-        mAuth = FirebaseAuth.getInstance();
+        if(txtFirstName.isEmpty()){
+            edtTxtFirstName.setError("Please enter your first name.");
+            edtTxtFirstName.requestFocus();
+        }
+        else if(txtLastName.isEmpty()){
+            edtTxtLastName.setError("Please enter your last name.");
+            edtTxtLastName.requestFocus();
+        }
+        else if(txtInstitution.isEmpty()){
+            edtTxtInstitution.setError("Please enter an institution.");
+            edtTxtInstitution.requestFocus();
+        }
+        else if(txtEmail.isEmpty()){
+            edtTxtEmail.setError("Please enter your email address.");
+            edtTxtEmail.requestFocus();
+        }
+        else if(txtPassword.isEmpty()){
+            edtTxtPassword.setError("Please enter your password.");
+            edtTxtPassword.requestFocus();
+        }
+        else {
 
-        Map<String, Object> dataToSave = new HashMap<String, Object>();
-        dataToSave.put("email", txtEmail);
-        dataToSave.put("firstName", txtFirstName);
-        dataToSave.put("lastName", txtLastName);
-        dataToSave.put("institution", txtInstitution);
-        dataToSave.put("userType", txtUserType);
-        dataToSave.put("username", txtFirstName + " " + txtLastName);
-        colRef.add(dataToSave);
+            mAuth = FirebaseAuth.getInstance();
 
-        mAuth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnCompleteListener(RegisterUser.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(!task.isSuccessful()){
-                    Toast.makeText(RegisterUser.this, "User Registration Failed", Toast.LENGTH_SHORT).show();
+            Map<String, Object> dataToSave = new HashMap<String, Object>();
+            dataToSave.put("email", txtEmail);
+            dataToSave.put("firstName", txtFirstName);
+            dataToSave.put("lastName", txtLastName);
+            dataToSave.put("institution", txtInstitution);
+            dataToSave.put("userType", txtUserType);
+            dataToSave.put("username", txtFirstName + " " + txtLastName);
+            colRef.add(dataToSave);
+
+            mAuth.createUserWithEmailAndPassword(txtEmail, txtPassword).addOnCompleteListener(RegisterUser.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        try{
+                            throw Objects.requireNonNull(task.getException());
+                        }
+                        catch(FirebaseAuthUserCollisionException emailExist){
+                            edtTxtEmail.setError("This email already exist, try another.");
+                            edtTxtEmail.requestFocus();
+                        } catch (Exception e) {
+                            Log.d("User Creation", e.getMessage());
+                        }
+                        Toast.makeText(RegisterUser.this, "User Registration Failed", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegisterUser.this, "User Registration Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterUser.this, MainActivity.class));
+                    }
                 }
-                else{
-                    startActivity(new Intent(RegisterUser.this, MainActivity.class));
-                }
-            }
-        });
+            });
 
-        txtPassword = "";
+            txtPassword = "";
+        }
 
     }
 
