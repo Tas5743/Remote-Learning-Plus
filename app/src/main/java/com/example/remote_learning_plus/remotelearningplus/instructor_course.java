d0db6443b84f049852c6d088649901788c7069d6
kgwint <73365790+kgwint@users.noreply.github.com>
2021-03-15 19:07:07 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/remote_learning_plus/remotelearningplus/instructor_course.java
@@ -0,0 +1,32 @@
+package com.example.remote_learning_plus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class instructor_course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_course);
+
+        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        bottomNavigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.btnHome:
+                        Intent intent = new Intent(getApplicationContext(), Instructor_HomePage.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+    }
+}
