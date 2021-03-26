eef8d91ee8b98ea9eae8e5290ba8a00cd0ae01e6
RoyalF4 <Royal2791@gmail.com>
2021-03-26 15:30:46 -0400

    - Merge Add Resources

--- app/src/main/java/com/example/remote_learning_plus/remotelearningplus/instructor_course.java
+++ app/src/main/java/com/example/remote_learning_plus/remotelearningplus/instructor_course.java
@@ -6,22 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class instructor_course extends AppCompatActivity {
+public class instructor_course extends AppCompatActivity implements View.OnClickListener {
     TextView courseName;
-    String courseID;
+    String courseRef,courseID,courseSection;
+    Button btnResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_course);
 
+        btnResources = findViewById(R.id.btnResources);
+        btnResources.setOnClickListener(this);
+
         Intent intent = getIntent();
         courseName = findViewById(R.id.tvCourse_Name);
         courseID = intent.getStringExtra("courseID");
+        courseSection = intent.getStringExtra("courseSelection");
+        courseRef = "courses/" + courseID + "/section/" + courseSection;
         courseName.setText(courseID);
 
 
@@ -38,5 +46,15 @@ public class instructor_course extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnResources:
+                Intent intent = new Intent(this, AddResource.class);
+                intent.putExtra("courseRef", courseRef);
+                startActivity(intent);
+        }
     }
 }
