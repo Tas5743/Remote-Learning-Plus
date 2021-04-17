2ab0bbfc90df45fb2b8392eef37f97e39d1b39a7
lmleonardo <70298072+lmleonardo@users.noreply.github.com>
2021-04-17 10:59:31 -0400

    Expanding resource function

---
+++ app/src/main/java/com/teamremote/remotelearningplus/ResourceModel.java
@@ -0,0 +1,28 @@
+package com.teamremote.remotelearningplus;
+
+public class ResourceModel {
+
+    public ResourceModel (){
+
+    }
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+
+    public void setURI(String URI) {
+        this.URI = URI;
+    }
+
+    private String URI;
+
+}
